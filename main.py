e03d78691d0b6715941ede20b4be68d3a9b685e2
shallon kobusinge <kobshallon041@gmail.com>
2022-01-31 16:44:16 +0200

    feat: simple switch case

--- main.py
+++ main.py
@@ -1,14 +1,78 @@
-# This is a sample Python script.
+# import json
+#
+# def login(usr):
+#     uN = input("Name: ")
+#     pW = input("Password: ")
+#
+#     if uN in usr.keys():
+#         if pW == usr[uN]:
+#             print("Welcome back.")
+#         else:
+#             print("Incorrect password.")
+#             return False
+#     else:
+#         print("Hello, new person.")
+#         usr[uN] = pW
+#
+#     writeUsers(usr)
+#     return True
+#
+# def readUsers():
+#     try:
+#         with open("users.json", "r") as f:
+#             return json.load(f)
+#     except FileNotFoundError:
+#         return {}
+#
+# def writeUsers(usr):
+#     with open("users.json", "w+") as f:
+#             json.dump(usr, f)
+#
+# users = readUsers()
+# success = login(users)
+#
+# while not success:
+#     success = login(users)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_input():
+    first_num = int(input("Enter the first number:      "))
+    second_num = int(input("Enter the second number:    "))
+    return first_num, second_num
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def add():
+    x, y = get_input()
+    return x + y
 
+
+def subtract():
+    x, y = get_input()
+    return x - y
+
+
+def multiply():
+    x, y = get_input()
+    return x * y
+
+
+def divide():
+    x, y = get_input()
+    return x // y
+
+
+def errorHandler():
+    return "Invalid choice"
+
+
+choice = int(input("Which operation do you want to carry out "))
+
+operations = {
+    1: add,
+    2: subtract,
+    3: multiply,
+    4: divide
+}
+
+output = operations.get(choice, errorHandler)()
+print(output)
